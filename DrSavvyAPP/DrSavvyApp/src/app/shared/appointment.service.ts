0995bb1b503de4effc54cae0971ad3cbc63fc3ab
Yusuf Hassim <hassim.yusuf@gmail.com>
2019-10-05 01:10:21 +0200

    mobile api deployed & API url updated in app

--- DrSavvyAPP/DrSavvyApp/src/app/shared/appointment.service.ts
+++ DrSavvyAPP/DrSavvyApp/src/app/shared/appointment.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AppointmentService {
-  apiURL = 'https://localhost:44335/api';
+  apiURL = 'https://drsavvyapi370.azurewebsites.net//api';
 
   // formData: Appointment;
   appoinments: Appointment[];
